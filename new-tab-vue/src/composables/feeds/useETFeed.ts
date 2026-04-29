a1fd838bc212914ef25d2ec44133724649935460
David <davidmitchellfootball@googlemail.com>
2026-04-30 00:06:42 +0100

    setting types more accurately

--- new-tab-vue/src/composables/feeds/useETFeed.ts
+++ new-tab-vue/src/composables/feeds/useETFeed.ts
@@ -9,7 +9,7 @@ export async function fetchEvangelicalTimesFeed(): Promise<RSSFeedItem[]> {
               if (cached) return cached
   const url = 'https://www.evangelical-times.org/rss/'
   const sourceName = 'ET'
-  const days = 14
+  const days = 7
 
   try {
     const api = `https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(url)}`
@@ -23,8 +23,13 @@ export async function fetchEvangelicalTimesFeed(): Promise<RSSFeedItem[]> {
 
     const result = data.items
                   .slice(0, 20)
-                  .map(item  => normalizePost(item, sourceName, days))
-            
+                .map(item  => { 
+                let np = normalizePost(item, sourceName, days)
+                const isPodcast = np.type.includes("Podcast")
+                const isReview = np.type.includes("Reviews")
+                const isArticle = np.type.includes("Articles")
+                return { ...np, genreType: isPodcast ? "Podcast" : isReview  ? "Review" : isArticle ? "Article" : np.genreType }
+              })
                 setCachedFeed(cacheKey, result)
                 return result
 
